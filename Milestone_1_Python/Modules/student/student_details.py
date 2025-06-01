fef340edffae095c41dc3edf00d660a092915e2b
Subham Sahoo <andharalauri@gmail.com>
2025-06-01 11:29:06 +0530

    added

---
+++ Milestone_1_Python/Modules/student/student_details.py
@@ -0,0 +1,13 @@
+# import os, sys
+# from os.path import dirname, join, abspath
+
+# parent_dir_path = abspath(join(dirname(__file__), ".."))
+# print("🚀 ~ parent_dir_path:", parent_dir_path)
+# sys.path.insert(0, parent_dir_path)
+# from teacher import teacher_details
+
+
+def student():
+    print('This is student details...')
+
+# teacher_details.teacher()
